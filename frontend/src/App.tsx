0176a98bb7bbd0269ff882891cfa87cbd17f079c
Sebastián Figueroa <sebaj348@gmail.com>
2025-08-12 20:14:18 -0400

    basic auth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,24 @@
 import "./App.css";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import Auth from "./pages/Auth";
+import AuthPage from "./pages/Auth";
 import Brainrot from "./pages/BrainrotPage";
 import ExplorePage from "./pages/ExplorePage";
 import HomePage from "./pages/HomePage";
 import MessagePage from "./pages/MessagesPage";
 import NotFoundPage from "./pages/NotFoundPage";
+import ProfilePage from "./pages/ProfilePage";
 
 function App() {
 	return (
 		<Router>
 			<Routes>
-				<Route path="/" element={<Auth />} />
+				<Route path="/" element={<AuthPage />} />
+				<Route path="/auth" />
 				<Route path="/home" element={<HomePage />} />
 				<Route path="/explore" element={<ExplorePage />} />
 				<Route path="/messages" element={<MessagePage />} />
 				<Route path="/brainrot" element={<Brainrot />} />
+				<Route path="/profile/*" element={<ProfilePage />} />
 				<Route path="*" element={<NotFoundPage />} />
 			</Routes>
 		</Router>
---
+++ frontend/src/auth/providers/metaProvider.ts
@@ -0,0 +1,5 @@
+function metaAuth(e: React.MouseEvent<HTMLButtonElement>) {
+	e.preventDefault();
+	console.log("logging with meta");
+}
+export default metaAuth;
