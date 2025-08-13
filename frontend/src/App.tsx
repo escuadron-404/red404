e3f9fcae7447a365ed8615aa2d7de7cc212866d6
Gabriel Prado <gaboalespana@gmail.com>
2025-08-12 22:19:09 -0400

    import SearchPage into App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
-import "./App.css";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import "./App.css";
 import Auth from "./pages/Auth";
 import Brainrot from "./pages/BrainrotPage";
 import ExplorePage from "./pages/ExplorePage";
 import HomePage from "./pages/HomePage";
 import MessagePage from "./pages/MessagesPage";
 import NotFoundPage from "./pages/NotFoundPage";
+import SearchPage from "./pages/SearchPage";
 
 function App() {
 	return (
@@ -13,6 +14,7 @@ function App() {
 			<Routes>
 				<Route path="/" element={<Auth />} />
 				<Route path="/home" element={<HomePage />} />
+				<Route path="/search" element={<SearchPage />} />
 				<Route path="/explore" element={<ExplorePage />} />
 				<Route path="/messages" element={<MessagePage />} />
 				<Route path="/brainrot" element={<Brainrot />} />
