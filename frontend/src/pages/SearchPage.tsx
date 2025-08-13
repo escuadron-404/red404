f2c160c9cf6da59984da92ec5ef29d1ce3ca71f8
Gabriel Prado <gaboalespana@gmail.com>
2025-08-12 23:37:08 -0400

    Remove import of NavBar inside homepages to import it only inside the layout

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,12 +1,7 @@
-import NavBar from "../components/NavBar";
-
 function SearchPage() {
-	return (
-		<div className="w-full h-screen flex bg-base">
-			<NavBar />
-			<main className="w-full h-screen flex pl-20 py-4">Coming soon...</main>
-		</div>
-	);
+  return (
+    <main className="w-full h-screen flex pl-20 py-4">Coming soon...</main>
+  );
 }
 
 export default SearchPage;
