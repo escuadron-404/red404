188a14287d7508ee93d0439c74945f2d14886cfa
Sebastián Figueroa <sebaj348@gmail.com>
2025-08-12 20:18:19 -0400

    added basic profile page

---
+++ frontend/src/components/UI/ProfileComponents/ContentContainer.tsx
@@ -0,0 +1,9 @@
+import type { ReactNode } from "react";
+
+interface ContentContainerProps {
+	children: ReactNode;
+}
+
+export default function ContentContainer(props: ContentContainerProps) {
+	return <div className="grid grid-cols-3 gap-2">{props.children}</div>;
+}
